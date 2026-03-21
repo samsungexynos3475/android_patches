6c7e91ce9f3bd34279dfc24e483507d8d3e49c38
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2026-03-21 13:12:00 +0000

    batteryextender: add patches for battery limit toggle
    LineageOS 18.1 does not natively have a charge limit. use this old unmerged extender hal for now

--- apply.sh
+++ apply.sh
@@ -3,6 +3,12 @@
 apply_msg "🎵 Audio blast fix"
 apply "hardware/samsung" "https://raw.githubusercontent.com/samsungexynos3475/android_patches/refs/heads/lineage-17.1/hardware_samsung/samsung-audio-Implement-auto-fade-in-to-suppress-AudioFlinger-volume-delay-blast.patch"
 
+apply_msg "🔋 Battery Extender"
+apply "device/lineage/sepolicy" "batteryextender/device_lineage_sepolicy/sepolicy-add-hal_lineage_batterylifeextender.patch"
+apply "hardware/lineage/interfaces" "batteryextender/hardware_lineage_interfaces/lineage-interfaces-add-batterylifeextender-HAL.patch"
+apply "hardware/samsung" "batteryextender/hardware_samsung/hidl-add-batterylifeextender-implementation.patch"
+apply "packages/apps/Settings" "batteryextender/packages_apps_Settings/Settings-add-Protect-battery-toggle.patch"
+
 apply_msg "🔵 Bluetooth SCO I2S routing"
 apply "system/bt" "https://raw.githubusercontent.com/samsungexynos3475/android_patches/refs/heads/lineage-17.1/system_bt/btm-fix-SCO-I2S-routing.patch"
 
