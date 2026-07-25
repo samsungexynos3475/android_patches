520c5104ec313f22b8465089419f896db54cc025
lavenly <135024283+lavenly@users.noreply.github.com>
2026-07-24 17:08:21 -0700

    lineage-18.1: add patches from 17.1

--- apply.sh
+++ apply.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+apply_msg "🎵 Audio blast fix"
+apply "hardware/samsung" "https://raw.githubusercontent.com/samsungexynos3475/android_patches/refs/heads/lineage-17.1/hardware_samsung/samsung-audio-Implement-auto-fade-in-to-suppress-AudioFlinger-volume-delay-blast.patch"
+
+apply_msg "🔵 Bluetooth SCO I2S routing"
+apply "system/bt" "https://raw.githubusercontent.com/samsungexynos3475/android_patches/refs/heads/lineage-17.1/system_bt/btm-fix-SCO-I2S-routing.patch"
+
 apply_msg "⚙️ Brightness float sync"
 apply "frameworks/base" "frameworks_base/core-Sync-float-brightness-from-int-setting-on-first-boot.patch"
 
