d02edb11c2f2ca1e29097765cfcf8ec4e1a5fd89
lavenly <135024283+lavenly@users.noreply.github.com>
2026-08-04 05:15:12 -0700

    keystore: add keystore backport patch

--- apply.sh
+++ apply.sh
@@ -8,3 +8,7 @@ apply "system/bt" "system_bt/btm-fix-SCO-I2S-routing.patch"
 
 apply_msg "✉️ Email crash fix"
 apply "packages/apps/UnifiedEmail" "packages_apps_UnifiedEmail/UnifiedEmail-Replace-incompatible-bitmap-drawables.patch"
+
+apply_msg "🔑 Keystore backports"
+apply "frameworks/base" "keystore/frameworks_base/keystore-backport-KeyStoreException.patch"
+apply "system/security" "keystore/system_security/keystore-silently-upgrade-key-blobs-during-attestation-to-bypass-KEY_REQUIRES_UPGRADE-errors.patch"
